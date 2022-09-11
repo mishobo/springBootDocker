ae0fce1e0043e65df73a5611a894ecab3197433f
mishobo <60912349+mishobo@users.noreply.github.com>
2022-09-12 00:00:03 +0300

    upload&Download APIs for files

---
+++ src/main/java/com/husseinabdallah287/springBootDocker/service/FileUploadUtil.java
@@ -0,0 +1,56 @@
+package com.husseinabdallah287.springBootDocker.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUploadUtil {
+	
+	private Path foundFile;
+
+    public static String saveFile(String fileName, MultipartFile multipartFile)
+            throws IOException {
+        Path uploadPath = Paths.get("E:\\projects\\fileUploads\\");
+          
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+ 
+        String fileCode = RandomStringUtils.randomAlphanumeric(8);
+         
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileCode + "-" + fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {       
+            throw new IOException("Could not save file: " + fileName, ioe);
+        }
+         
+        return fileCode;
+    }
+	
+    public Resource getFileAsResource(String fileCode) throws IOException {
+        Path dirPath = Paths.get("E:\\projects\\fileUploads\\");
+         
+        Files.list(dirPath).forEach(file -> {
+            if (file.getFileName().toString().startsWith(fileCode)) {
+                foundFile = file;
+                return;
+            }
+        });
+ 
+        if (foundFile != null) {
+            return new UrlResource(foundFile.toUri());
+        }
+         
+        return null;
+    }
+	
+}
